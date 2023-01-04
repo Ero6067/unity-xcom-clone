7c6cbf7a11a7b250624cfa55634a6491a5805931
Kris <kris.lafrance6067@gmail.com>
2023-01-04 15:18:39 -0800

    Adding debug information pulled from GridDebugObject.cs

--- Assets/Scripts/Grid/GridObject.cs
+++ Assets/Scripts/Grid/GridObject.cs
@@ -12,4 +12,8 @@ public class GridObject
         this.gridSystem = gridSystem;
         this.gridPosition = gridPosition;
     }
+    public override string ToString()
+    {
+        return gridPosition.ToString();
+    }
 }
