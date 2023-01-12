4a37ed180ea9d0f62d868e7606445ee3cee4d20e
Kris <kris.lafrance6067@gmail.com>
2023-01-11 17:33:11 -0800

    Adding action base class and refactor

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,3 +1,9 @@
+/*
+ * 
+ * 
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +13,12 @@ public class Unit : MonoBehaviour
 
     private GridPosition gridPosition;
     private MoveAction moveAction;
+    private SpinAction spinAction;
 
     private void Awake()
     {
         moveAction = GetComponent<MoveAction>();
+        spinAction = GetComponent<SpinAction>();
     }
 
     private void Start()
@@ -33,6 +41,11 @@ public class Unit : MonoBehaviour
     public MoveAction GetMoveAction()
     {
         return moveAction;
+    }
+
+    public SpinAction GetSpinAction()
+    {
+        return spinAction;
     }
 
     public GridPosition GetGridPosition()
